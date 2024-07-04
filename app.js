792175b0207f19de07f9e5dd55de4a14863dcfe2
eoydev <anikslin@gmail.com>
2024-07-04 14:11:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+const DemoApp = {
+    init: function() {
+        // Инициализация приложения
+    },
+
+    sendMessage: function(message, webviewButton) {
+        // Отправка сообщения
+    },
+
+    sendTime: function(multiplier) {
+        // Отправка текущего времени
+    },
+
+    expand: function() {
+        // Расширение веб-вью
+    },
+
+    toggleMainButton: function(button) {
+        // Скрытие/показ главной кнопки
+    },
+
+    requestLocation: function(link) {
+        // Запрос местоположения
+    },
+
+    requestVideo: function(link) {
+        // Запрос видео
+    },
+
+    requestAudio: function(link) {
+        // Запрос аудио
+    },
+
+    requestAudioVideo: function(link) {
+        // Запрос аудио и видео
+    },
+
+    testClipboard: function(link) {
+        // Чтение из буфера обмена
+    },
+
+    showAlert: function(message) {
+        // Показ алерта
+    },
+
+    showConfirm: function(message) {
+        // Показ подтверждения
+    },
+
+    requestWriteAccess: function() {
+        // Запрос доступа на запись
+    },
+
+    requestContact: function() {
+        // Запрос контактов
+    },
+
+    showPopup: function() {
+        // Показ всплывающего окна
+    },
+
+    showScanQrPopup: function(onlyLinks) {
+        // Показ окна сканирования QR-кода
+    }
+};
